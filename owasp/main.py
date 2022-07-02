0e1435ab69c8dc2a8a3e40f62db89e003db0a4ff
loghacker <loghacker666@gmail.com>
2022-07-02 12:49:43 +0530

    xss added

--- owasp/main.py
+++ owasp/main.py
@@ -6,6 +6,7 @@ try:
     print("""
     Choose option:
         1. Sql injection
+        2. XSS injection
     """)
     a = int(input("Enter here: "))
 
@@ -13,6 +14,10 @@ try:
         print("Entering sql injection....")
         system('clear')
         system('./owasp/sqli.py')
+    elif a == 2:
+        print("Entering XSS injection....")
+        system('clear')
+        system('./owasp/xxs.py')
     else:
         print('wrong entry')
 
@@ -20,6 +25,7 @@ except KeyboardInterrupt:
     a=input("Do you want to EXIT (y/n): ")
     if a=='n' or a=='N':
         system('./owasp/main.py')
+
     else:
         print("Exiting...")
 
